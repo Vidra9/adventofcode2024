067e062a1b8be527304de7cf7fe57ee73b1845a5
Mihailo Vidakovic <mihailo.system@gmail.com>
2024-12-11 17:02:25 +0100

    Day1 solution

--- day1.cpp
+++ day1.cpp
@@ -3,6 +3,39 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <map>
+
+int solvePart1(std::vector<int> leftList, std::vector<int> rightList)
+{
+    std::sort(leftList.begin(), leftList.end(), [](int left, int right){return left <= right;});
+    std::sort(rightList.begin(), rightList.end(), [](int left, int right){return left <= right;});
+
+    int total = 0;
+    for (int i = 0; i < leftList.size(); i++)
+    {
+        total += abs(leftList[i] - rightList[i]);
+    }
+
+    return total;
+}
+
+int solvePart2(std::vector<int> leftList, std::vector<int> rightList)
+{
+    std::map<int, int> occuranceMap;
+    for (int number : rightList)
+    {
+        occuranceMap.try_emplace(number, 0);
+        occuranceMap[number]++;
+    }
+
+    int total = 0;
+    for (int number : leftList)
+    {
+        total += number * occuranceMap[number];
+    }
+
+    return total;
+}
 
 int main()
 {
@@ -17,16 +50,8 @@ int main()
         list2.push_back(std::stoi(line.substr(line.rfind(" ") + 1, line.length())));
     }
 
-    std::sort(list1.begin(), list1.end(), [](int left, int right){return left <= right;});
-    std::sort(list2.begin(), list2.end(), [](int left, int right){return left <= right;});
-
-    int total = 0;
-    for (int i = 0; i < list1.size(); i++)
-    {
-        total += abs(list1[i] - list2[i]);
-    }
-
-    printf("%d", total);
+    printf("Part 1 output: %d \n", solvePart1(list1, list2));
+    printf("Part 2 output: %d \n", solvePart2(list1, list2));
 
     return 0;
 }
