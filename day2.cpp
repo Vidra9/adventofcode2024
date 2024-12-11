9e95a4784284b887e16f548d4679559027f63508
Mihailo Vidakovic <mihailo.system@gmail.com>
2024-12-11 21:44:42 +0100

    day 2 part 2 solved (brute force)

--- day2.cpp
+++ day2.cpp
@@ -22,101 +22,94 @@ int solvePart1(std::vector<std::vector<int>> _reports)
     return safe;
 }
 
+int FindReportError(std::vector<int> rep)
+{
+    bool descending = rep[0] > rep[1];
+    for (int i = 1; i < rep.size() - 1; i++)
+    {
+        if (abs(rep[i-1] - rep[i]) > 3 || abs(rep[i-1] - rep[i]) == 0) return i;
+        if (abs(rep[i+1] - rep[i]) > 3 || abs(rep[i+1] - rep[i]) == 0) return i;
+        if (descending)
+        {
+            if(rep[i-1] < rep[i] || rep[i] < rep[i+1]) return i;
+        }
+        else
+        {
+            if(rep[i-1] > rep[i] || rep[i] > rep[i+1]) return i;
+        }
+    }
+    return -1;
+}
+
+void printVec(std::vector<int> vec , int error_index)
+{
+    for(int i = 0; i < vec.size(); i++)
+    {
+        printf("%d ", vec[i]);
+        if (i + 1 != vec.size() && (abs(vec[i] - vec[i+1]) > 3 || abs(vec[i] - vec[i+1]) == 0))
+        {
+            printf("- |%d| - ", abs(vec[i] - vec[i+1]));
+        }
+    }
+    printf(" <- error_index: %d\n", error_index);
+}
+
 // One bad "level" in report allowed
-// Completely fucked - too many false safes???
+// After hours of debugging brute forcing the check on elements around the error seems like the only decent solution
 int solvePart2(std::vector<std::vector<int>> _reports)
 {
     int safe = 0;
     for (std::vector<int> _report : _reports)
     {
-        int last_counter = 0;
-        int last = _report[last_counter];
-        int current_counter = 1;
-        int current = _report[current_counter];
-
-        bool is_safe = true;
-        bool error_found = false;
-        bool descending = false;
-
-        if (abs(last - current) > 3 || abs(last - current) == 0)
+        int error_index = FindReportError(_report);
+        std::vector<int> temp;
+        if (error_index == -1) 
         {
-            error_found = true;
-            last = current;
-            last_counter = current_counter;
-            current = _report[++current_counter];
+            safe++;
         }
-
-        if (last > current) descending = true;
-        else if (last == current) // Handle case where first two are equal
+        else
         {
-            error_found = true;
-            last = current;
-            last_counter = current_counter;
-            current = _report[++current_counter];
-            descending = last > current;
-        }
-
-        while (current_counter < _report.size())
-        {
-            if (abs(last - current) == 0)
+            for (int i = 0; i < _report.size(); i++)
             {
-                if (error_found)
-                {
-                    is_safe = false;
-                    break;
-                }
-                else 
-                {
-                    error_found = true;
-                    // printf("error found: order: %s | %d[%d] : %d[%d] \n", descending ? "DESC" : "ASC", _report[last_counter], last_counter, _report[current_counter], current_counter);
-                    current = _report[++current_counter];
-                    continue;
-                }
-            } else if (abs(last - current) > 3)
-            {
-                if (error_found)
-                {
-                    is_safe = false;
-                    break;
-                }
-                else 
-                {
-                    error_found = true;
-                    // printf("error found: order: %s | %d[%d] : %d[%d] \n", descending ? "DESC" : "ASC", _report[last_counter], last_counter, _report[current_counter], current_counter);
-                    current = _report[++current_counter];
-                    continue;
-                }
+                if (i != error_index + 1)
+                    temp.push_back(_report[i]);
             }
-            else if ((descending && last < current) || (!descending && last > current))
+
+            if(FindReportError(temp) == -1)
             {
-                if (error_found)
+                safe++;
+                continue;
+            }
+            else
+            {
+                temp.clear();
+                for (int i = 0; i < _report.size(); i++)
                 {
-                    is_safe = false;
-                    break;
+                    if (i != error_index)
+                        temp.push_back(_report[i]);
+                }
+                
+                if(FindReportError(temp) == -1)
+                {
+                    safe++;
+                    continue;
                 }
                 else
                 {
-                    error_found = true;
-                    // printf("error found: order: %s | %d[%d] : %d[%d] \n", descending ? "DESC" : "ASC", _report[last_counter], last_counter, _report[current_counter], current_counter);
-
-                    current = _report[++current_counter];
-                    descending = last > current;
-                    continue;
+                    temp.clear();
+                    for (int i = 0; i < _report.size(); i++)
+                    {
+                        if (i != error_index - 1)
+                            temp.push_back(_report[i]);
+                    }
+                    
+                    if(FindReportError(temp) == -1)
+                    {
+                        safe++;
+                        continue;
+                    }                    
                 }
             }
-            last_counter = current_counter;
-            last = current;
-            current = _report[++current_counter];
-        }
-        if (is_safe) safe++;
-        if (error_found && false)
-        {
-            printf("Report: ");
-            for (int num : _report)
-            {
-                printf("%d ", num);
-            }
-            printf("- %s \n", is_safe ? "SAFE" : "UNSAFE");
         }
     }
 
@@ -143,9 +136,9 @@ int main()
         reports.push_back(report);
     }
 
-    printf("Part 1 output :%d \n", solvePart1(reports));
+    printf("Part 1 output : %d \n", solvePart1(reports));
 
-    printf("Part 2 output :%d \n", solvePart2(reports));
+    printf("Part 2 output : %d \n", solvePart2(reports));
 
 
     return 0;
