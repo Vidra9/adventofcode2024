1e969ce4f03f636f41095e3d04f292a259171f55
Mihailo Vidakovic <mihailo.system@gmail.com>
2024-12-11 19:10:16 +0100

    too high day 2 part 2 "solution"

--- day2.cpp
+++ day2.cpp
@@ -22,6 +22,107 @@ int solvePart1(std::vector<std::vector<int>> _reports)
     return safe;
 }
 
+// One bad "level" in report allowed
+// Completely fucked - too many false safes???
+int solvePart2(std::vector<std::vector<int>> _reports)
+{
+    int safe = 0;
+    for (std::vector<int> _report : _reports)
+    {
+        int last_counter = 0;
+        int last = _report[last_counter];
+        int current_counter = 1;
+        int current = _report[current_counter];
+
+        bool is_safe = true;
+        bool error_found = false;
+        bool descending = false;
+
+        if (abs(last - current) > 3 || abs(last - current) == 0)
+        {
+            error_found = true;
+            last = current;
+            last_counter = current_counter;
+            current = _report[++current_counter];
+        }
+
+        if (last > current) descending = true;
+        else if (last == current) // Handle case where first two are equal
+        {
+            error_found = true;
+            last = current;
+            last_counter = current_counter;
+            current = _report[++current_counter];
+            descending = last > current;
+        }
+
+        while (current_counter < _report.size())
+        {
+            if (abs(last - current) == 0)
+            {
+                if (error_found)
+                {
+                    is_safe = false;
+                    break;
+                }
+                else 
+                {
+                    error_found = true;
+                    // printf("error found: order: %s | %d[%d] : %d[%d] \n", descending ? "DESC" : "ASC", _report[last_counter], last_counter, _report[current_counter], current_counter);
+                    current = _report[++current_counter];
+                    continue;
+                }
+            } else if (abs(last - current) > 3)
+            {
+                if (error_found)
+                {
+                    is_safe = false;
+                    break;
+                }
+                else 
+                {
+                    error_found = true;
+                    // printf("error found: order: %s | %d[%d] : %d[%d] \n", descending ? "DESC" : "ASC", _report[last_counter], last_counter, _report[current_counter], current_counter);
+                    current = _report[++current_counter];
+                    continue;
+                }
+            }
+            else if ((descending && last < current) || (!descending && last > current))
+            {
+                if (error_found)
+                {
+                    is_safe = false;
+                    break;
+                }
+                else
+                {
+                    error_found = true;
+                    // printf("error found: order: %s | %d[%d] : %d[%d] \n", descending ? "DESC" : "ASC", _report[last_counter], last_counter, _report[current_counter], current_counter);
+
+                    current = _report[++current_counter];
+                    descending = last > current;
+                    continue;
+                }
+            }
+            last_counter = current_counter;
+            last = current;
+            current = _report[++current_counter];
+        }
+        if (is_safe) safe++;
+        if (error_found && false)
+        {
+            printf("Report: ");
+            for (int num : _report)
+            {
+                printf("%d ", num);
+            }
+            printf("- %s \n", is_safe ? "SAFE" : "UNSAFE");
+        }
+    }
+
+    return safe;
+}
+
 int main()
 {
     std::ifstream inputFile("input2.txt");
@@ -43,6 +144,9 @@ int main()
     }
 
     printf("Part 1 output :%d \n", solvePart1(reports));
+
+    printf("Part 2 output :%d \n", solvePart2(reports));
+
 
     return 0;
 }
