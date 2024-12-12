88a4d5fe26137988057836a8cc413efcadee8ca3
Mihailo Vidakovic <mihailo.system@gmail.com>
2024-12-12 19:34:19 +0100

    day 3 part 1 complete (file.get + atoi)

--- day3.cpp
+++ day3.cpp
@@ -13,80 +13,43 @@ int main()
 
     while(input.ignore(256, 'm'))
     {
-        // int c = input.peek();
-        
-        // refactor to maybe get c?
+        char c;
 
-        sb->pubseekoff(-1, input.cur);
-        // input.read(str, 11);
-        std::streamsize size = sb->pubseekoff(4, input.cur); // ) - breaks if out of file
-        sb->pubseekoff(-4, input.cur); // this stream is stupid
-        char* str = new char [size];
-        sb->sgetn(str, size);
-        // input.get(c);
-        std::string part(str);
-        std::string temp;
-        
-        std::cout<<"part : "<<part<<std::endl;
-        // temp = part.substr(0,4);
-        // if (temp != "mul(") continue;
-        // int comma = part.find(',');
-        // // std::cout<<"comma : "<<comma<<std::endl;
-        // temp = part.substr(4, comma - 4);
-        // // std::cout<<"temp before left "<<temp<<std::endl;
-        // int leftNum = std::stoi(temp);
-        // temp = part.substr(comma + 1, part.find(')') - comma - 1);
-        // // std::cout<<"temp before right "<<temp<<std::endl;
-        // int rightNum = std::stoi(temp);
+        input.get(c);
+        if (c != 'u') continue;
+        input.get(c);
+        if (c != 'l') continue;
+        input.get(c);
+        if (c != '(') continue;
 
-        // std::cout<<"left: "<<leftNum<<" right: "<<rightNum<<std::endl;
-        // output += leftNum * rightNum;
-        bool badPattern = false;
-
-        if(part == "mul(")
+        bool isBad = false;
+        char* left = new char [3];
+        int count = 0;
+        char* right = new char [3];
+        while(input.get(c))
         {
-            std::string left, right;
-            char c;
-            while(input.get(c))
-            {
-                std::cout<<"we're fucked \n";
-                if (isdigit(c))
-                {
-                    left.push_back(c);
-                }
-                else if (c == ',')
-                {
-                    break;
-                }
-                else
-                {
-                    badPattern = true;
-                    break;
-                }
-            }
-            if(badPattern) continue;
-            while(input.get(c))
-            {
-                if (isdigit(c))
-                {
-                    right.push_back(c);
-                }
-                else if (c == ')')
-                {
-                    break;
-                }
-                else
-                {
-                    badPattern = true;
-                    break;
-                }
-            }
-            std::cout<<"left: "<<left<<" | right: "<<right<<std::endl;
+            if(isdigit(c)) { left[count++] = c;}
+            else if(c == ',') break;
+            else {isBad = true; break;}
         }
-
-        // if (c == EOF) break;
-
-        if(max-- == 0) break;
+        if (isBad) continue;
+        else
+        {
+            isBad = false;
+            count = 0;
+            while(input.get(c))
+            {
+                if(isdigit(c)) right[count++] = c;
+                else if(c == ')') break;
+                else {isBad = true; break;}
+            }
+            if (isBad) continue;
+            else
+            {
+                printf("%d * %d \n", std::atoi(left), std::atoi(right));
+                output += std::atoi(left) * std::atoi(right);
+            }
+        }
     }
     
     printf("%d", output);
