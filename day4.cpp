7d73803f71ffabc7b9c2d441bb08f0492f1c3ea1
Mihailo Vidakovic <mihailo.system@gmail.com>
2024-12-13 17:26:15 +0100

    Day 4 Part 2

--- day4.cpp
+++ day4.cpp
@@ -122,9 +122,40 @@ int solvePart1(std::vector<std::string> lines)
     return total;
 }
 
+bool checkCrossMAS(std::vector<std::string>& lines, coord s_coord)
+{
+    int x = s_coord.x;
+    int y = s_coord.y;
+    bool first_diag = false;
+
+    if(x < 1 || x + 1 >= lines.size() || y < 1 || y + 1 >= lines[x].size()) return false;
+
+    // check first diagonal
+    if((lines[x-1][y-1] == 'M' && lines[x+1][y+1] == 'S') 
+    || (lines[x-1][y-1] == 'S' && lines[x+1][y+1] == 'M')) first_diag = true;
+
+    if (((lines[x-1][y+1] == 'M' && lines[x+1][y-1] == 'S') 
+    || (lines[x-1][y+1] == 'S' && lines[x+1][y-1] == 'M')) && first_diag) return true;
+
+    return false;
+}
+
 int solvePart2(std::vector<std::string> lines)
 {
-    return 0;
+    int total = 0;
+    std::vector<coord> used_a;
+
+    for(int i = 0; i < lines.size(); i++)
+    {
+        for(int j = 0; j < lines[i].size(); j++)
+        {
+            char x = lines[i][j];
+            if (x != 'A' && !vectorContains(used_a, coord{i, j})) continue;
+            if(checkCrossMAS(lines, coord{i, j})) total++;
+        }
+    }
+
+    return total;
 }
 
 int main()
@@ -138,7 +169,8 @@ int main()
         _lines.push_back(line);
     }
 
-    printf("Part 1: %d", solvePart1(_lines));
+    printf("Part 1: %d \n", solvePart1(_lines));
+    printf("Part 2: %d \n", solvePart2(_lines));
 
     return 0;
 }
